9c4c903ae5688f20458207a4f8ed93b68492b90a
Ellie <hellonewman@users.noreply.github.com>
2019-06-17 21:56:41 -0400

    Add files via upload

---
+++ 01-mapping-stations.R
@@ -0,0 +1,69 @@
+# Install leaflet (this is only required once)
+install.packages('leaflet', dependencies = TRUE)
+
+# Load library
+library(leaflet)
+
+# Read in MBTA Station txt file
+rawlocs <- read.csv(file="./stops.txt", head=TRUE,sep=",")
+View(rawlocs)
+
+# Select columns with MBTA T stations
+station_locs <- rawlocs
+
+#Select the columns we want and change columns name to latitude and longitude
+station_locs <- station_locs[c("stop_id","stop_name","stop_lat","stop_lon")]
+colnames(station_locs)[3] <- "latitude"
+colnames(station_locs)[4] <- "longitude"
+
+# Convert the columns imported as a factor to characters
+station_locs$stop_id <- as.character(station_locs$stop_id)
+station_locs$stop_name <- as.character(station_locs$stop_name)
+
+# Remove all the rows which do not contain numbers
+station_locs <- station_locs[!is.na(as.numeric(station_locs$stop_id)), ]
+
+# Convert the Stop ID column into numbers
+station_locs$stop_id = as.numeric(station_locs$stop_id)
+
+# Select columns with MBTA T stations
+station_locs <- station_locs[which ((station_locs$stop_id >= 70000) & (station_locs$stop_id <= 70279) ),]
+
+
+# Saint Paul Street Station names are altered to include their line.
+# This change is done to be able to distinguish the two stations
+# named Saint Paul Street on the B and C line.
+
+station_locs$stop_name[station_locs$stop_id == 70140] <-  "Saint Paul Street B Line"
+station_locs$stop_name[station_locs$stop_id == 70141] <-  "Saint Paul Street B Line"
+station_locs$stop_name[station_locs$stop_id == 70217] <-  "Saint Paul Street C Line"
+station_locs$stop_name[station_locs$stop_id == 70218] <-  "Saint Paul Street C Line"
+
+# Most T stations are defined by multiple station platforms, 
+# which might not be at the same latitude/longitude. 
+# Find the unique station names so we can get one observation per station
+station_locs <- station_locs[!duplicated(station_locs$stop_name),]
+
+# Select the rows which do not have Outbound in the text
+# Remove string with dash
+station_locs$stop_name <- sub("\\-.*","",station_locs$stop_name)
+
+#Remove and extra spaces
+station_locs$stop_name <- trimws(station_locs$stop_name,which = c("right"))
+View(station_locs)
+
+#export the data to a csv file
+write.csv(station_locs, "./mbta_stops.csv")
+
+# Map the stations
+# Lat Long corrdinates from www.latlong.net
+mbta_subway <- leaflet(station_locs) %>%
+  addTiles() %>%  
+  setView(-71.057083, 42.361145, zoom = 12) %>%
+  addCircles(~longitude, ~latitude, weight = 3, radius=120, 
+                 color="#0073B2", stroke = TRUE, fillOpacity = 0.8) %>% 
+  addLegend("bottomleft", colors="#0073B2", labels="Data Source: MBTA Developer Portal", title="MBTA Subway Stations")
+  
+# show the map
+mbta_subway  
+
---
+++ 03-reliability.R
@@ -0,0 +1,69 @@
+# Install leaflet (this is only required once)
+install.packages('leaflet', dependencies = TRUE)
+
+# Load library
+library(leaflet)
+
+#read in MBTA performance csv file
+rawdata <- read.csv(file="./TDashboardData_reliability_20160301-20160331.csv", head=TRUE,sep=",")
+View(rawdata)
+
+#Select Peak Service rows for the Green Line
+reliability <- rawdata[which (rawdata$PEAK_OFFPEAK_IND =="Peak Service (Weekdays 6:30-9:30AM, 3:30PM-6:30PM)"),]
+View(reliability)
+
+#Remove columns of data we don't need
+reliability <- reliability[c("SERVICE_DATE","ROUTE_TYPE","STOP","OTP_NUMERATOR","OTP_DENOMINATOR")]
+
+#Select only the Green Line data
+reliability <- reliability[which (reliability$ROUTE_TYPE=='Green Line'),]
+
+#Remove and extra spaces
+reliability$STOP <- trimws(reliability$STOP,which = c("right"))
+
+# Find the reliability at each station
+# Divide the numerator (people who has to wait too long) by the denominator (all riders).
+reliability$rely <- reliability$OTP_NUMERATOR / reliability$OTP_DENOMINATOR
+# This is the percentage of people who had to wait. 1 - this ratio is the percentage of people who didn't.
+reliability$rely <- 1 - reliability$rely
+# Round by 4
+reliability$rely <- round(reliability$rely, digits=4)
+View(reliability)
+
+# Import the MBTA station location data
+stations <- read.csv("./mbta_stations.csv")
+View(stations)
+
+# Select only the Green Line data
+locs <- stations[which (stations$LINE=='GREEN'),]
+
+# Change the name of Station to Stop to match the other dataframe
+colnames(locs)[2] <- "STOP" 
+View(locs)
+
+
+# Join the location data to the reliability file
+joindata <- merge(x = reliability, y = locs, by ="STOP", all.x=TRUE)
+
+#Select only the columns need
+joindata <- joindata[c("STOP","SERVICE_DATE","ROUTE_TYPE","rely","LONGITUDE","LATITUDE")]
+x = !duplicated(joindata$STOP)
+joindata <- joindata[x ,]
+View(joindata)
+
+# Map the Green Line
+# Map the stations
+# Create a ColorBrewer Greens color palette. 
+colorpal <- colorNumeric(palette = "Greens", domain = joindata$rely)
+
+
+# Lat Long corrdinates from www.latlong.net
+mbta_subway <- leaflet(joindata) %>%
+  addTiles() %>%  
+  setView(-71.057083, 42.361145, zoom = 12) %>%
+  addCircles(~LONGITUDE, ~LATITUDE, weight = 3, radius=120, 
+             color=~colorpal(rely), stroke = TRUE, fillOpacity = 0.8) %>% 
+  addLegend("topright", colorpal, values=~rely, title="MBTA Green Line Reliability, Data Source: MBTA Developer Portal")
+
+# Show the map
+mbta_subway  
